1671fed64d202d94d16713a14ad7b8f5c2278dca
Pingvu <nhlpool27@gmail.com>
2023-03-17 14:18:17 +0100

    Add project files.

---
+++ OV3/Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+
+namespace Number
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            
+            for (int i = 100; i >= 1; i--)
+            {
+                Console.WriteLine(i);
+            }
+
+            
+            Console.WriteLine("Tryck på en tangent för att avsluta...");
+          
+        }
+    }
+}
